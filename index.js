1c1e5496b13fdd1c17851148d0455fe35006f006
linazoo <linazooper@gmail.com>
2018-05-29 22:16:16 -0500

    started thinking about how to select each progress bar and add or subtract at the same time. remove event listener not working

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 (function(){
   const meter1 = document.getElementById('meter1'); 
   const plusFiveBtn = document.querySelector('.plus-10');
-  
+  const meterSelector = document.querySelector('.progress-selector');
+
   plusFiveBtn.addEventListener('click', function() {
     meter1.value += 10;
+  })
+
+  meterSelector.addEventListener('change', function() {
+
   })
 })();
